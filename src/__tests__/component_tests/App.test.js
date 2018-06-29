37ec6fdf5659f89a1d5794f7a7ee0f760c4de7fd
Joshua Holloway <josh.holloway@live.co.uk>
2018-06-29 12:36:55 +0100

    restructured tests due to new headers component in creation

--- src/__tests__/component_tests/App.test.js
+++ src/__tests__/component_tests/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './../../components/App';
-import styled from 'styled-components';
 
 const app = shallow(<App/>);
 
@@ -14,14 +13,10 @@ it('renders without crashing', () => {
 
 describe('passing props', () => {
 
-  describe('Header Component', () => {
+  describe('Headers Component', () => {
 
-    it('has header 1', () => {
-      expect(app.find('Header').at(0).prop('title')).toMatchObject(<styled.h1>Ned Ned's Cheese Emporium</styled.h1>);
-    });
-
-    it('has header 2', () => {
-      expect(app.find('Header').at(1).prop('title')).toMatchObject(<styled.h2>Today you can buy 12 blocks of cheese for £19.99 🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀</styled.h2>);
+    it('has headers', () => {
+      expect(app.find('Headers'));
     });
   });
 });
---
+++ src/__tests__/component_tests/Headers.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import Headers from './../../components/Headers';
+import styled from 'styled-components';
+
+const app = shallow(<Headers/>);
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Headers />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('passing props', () => {
+
+  describe('Header Component', () => {
+
+    it('has header 1', () => {
+      expect(app.find('Header').at(0).prop('title')).toMatchObject(<styled.h1>Ned Ned's Cheese Emporium</styled.h1>);
+    });
+
+    it('has header 2', () => {
+      expect(app.find('Header').at(1).prop('title')).toMatchObject(<styled.h2>Today you can buy 12 blocks of cheese for £19.99 🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀</styled.h2>);
+    });
+  });
+});
