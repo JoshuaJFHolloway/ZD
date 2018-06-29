b24e47cdff61359ca63bd4cf1afabdd316ccbbdb
Joshua Holloway <josh.holloway@live.co.uk>
2018-06-29 10:01:31 +0100

    :fire: deleted all unneccesary code in create react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Ned Ned's Cheese Emporium</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <span role={"img"} aria-label={"emoji"} >Today you can buy 12 blocks of cheese for £19.99 🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀</span>
       </div>
     );
   }
