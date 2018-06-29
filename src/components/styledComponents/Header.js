916b48db376f379e89771f100376780e8b0c0376
Joshua Holloway <josh.holloway@live.co.uk>
2018-06-29 12:21:34 +0100

    created header component and used styled components for styling

---
+++ src/components/styledComponents/Header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const HeaderStyle = styled.h1 `
+  font-size: 30px;
+  font-weight: bolder;
+  font-family: Helvetica;
+  line-height: 1.5;
+`;
+
+export default HeaderStyle;
---
+++ src/components/styledComponents/subHeader.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const SubHeaderStyle = styled.h2 `
+  font-size: 20px;
+  font-weight: normal;
+  font-family: Helvetica;
+  line-height: 1.5;
+`;
+
+export default SubHeaderStyle;
