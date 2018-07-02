89e00f3f0bfc128fd29c8a9e185f1769fb02008d
Joshua Holloway <josh.holloway@live.co.uk>
2018-07-02 11:45:38 +0100

    :white_check_mark: test passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react';
 import Headers from './Headers';
-import Inputs from './Inputs';
-import Results from './Results';
+import InputsHolder from './InputsHolder';
+import ResultsHolder from './ResultsHolder';
+import {fetchData} from '../utils/fetchData';
+import TwoColumnStyle from './styledComponents/TwoColumns';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      params: {
+        cheeseMoney: false,
+        buttonClicked: false,
+      },
+      results: {},
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.getData = this.getData.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    const params = this.state.params;
+
+      params[name]= value;
+
+    if (params.buttonClicked) {
+      this.setState({params})
+    }
+  }
+
+
   render() {
     return (
       <div>
         <Headers/>
-        <Inputs/>
-        <Results/>
+        <div>
+
+            <InputsHolder
+              handleClick={this.getData}
+              handleChange={this.handleChange}
+              type={"number"}
+              inputName={"cheeseMoney"}
+              buttonName={"Convert GBP to cheese"}
+              title={"Enter in your amount in GBP"}
+            />
+
+
+            <ResultsHolder
+              state={this.state}
+            />
+
+        </div>
       </div>
     );
   }
