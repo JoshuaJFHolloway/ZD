35b89269ba537e63db7ef01364542de3a17d14b5
Joshua Holloway <josh.holloway@live.co.uk>
2018-07-02 11:59:21 +0100

    altered InputsHolder to handle props correctly

---
+++ src/components/InputsHolder.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from './Header';
+import InputTitleStyle from './styledComponents/inputTitle';
+import ButtonStyle from './styledComponents/Button';
+import InputStyle from './styledComponents/Input';
+
+const InputsHolder = (props) => {
+  return (
+    <div>
+      <InputTitleStyle>
+        <Header
+          title={props.title}
+        />
+      </InputTitleStyle>
+      <InputStyle
+        type={props.type}
+        name={props.inputName}
+        onChange={props.handleChange}/>
+      <ButtonStyle
+        name={props.buttonName}
+        onClick={props.handleClick}
+      >
+        {props.buttonName}
+      </ButtonStyle>
+    </div>
+  )
+};
+
+export default InputsHolder;
