9b1a53ccfa9165c69b5ce9d5079d539d2bc9885c
Joshua Holloway <josh.holloway@live.co.uk>
2018-07-02 11:45:20 +0100

    adding test for handleChange

--- src/__tests__/component_tests/App.test.js
+++ src/__tests__/component_tests/App.test.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import App from './../../components/App';
+import sinon from 'sinon';
 
-const app = shallow(<App/>);
+import * as fetchUtils from '../../utils/fetchData';
+import {dummyResponse} from '../../utils/mockData/response';
+
+let app;
+
 
 describe('App', () => {
+
+
+  beforeEach(() => {
+    app = shallow(<App/>);
+
+  });
 
   it('renders correctly', () => {
     expect(app).toMatchSnapshot();
   });
 
-  describe('passing props', () => {
-
-    describe('Headers Component', () => {
+  describe('rendering components', () => {
 
       it('has headers', () => {
         expect(app.find('Headers'));
       });
-    });
+
+      it('has InputsHolder', () => {
+        expect(app.find('InputsHolder'));
+      });
+
+      it('has ResultsHolder', () => {
+        expect(app.find('ResultsHolder'));
+      });
   });
+
+  describe('Updating state successfully', () => {
+
+    describe('handleChange', () => {
+
+      it('updates the state correctly depending on the event.target', () => {
+        app.instance().state.params.buttonClicked = true;
+        app.instance().handleChange(handleChangeEvent);
+        expect(app.instance().state.params.cheeseMoney).toEqual('20');
+      });
+    });
+  })
+
 });
