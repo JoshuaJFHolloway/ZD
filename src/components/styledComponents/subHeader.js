f0aab53ad9cb509551961a1147f4be7006e861ac
Joshua Holloway <josh.holloway@live.co.uk>
2018-07-02 12:02:37 +0100

    modified styling of component

--- src/components/styledComponents/subHeader.js
+++ src/components/styledComponents/subHeader.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const SubHeaderStyle = styled.h2 `
   font-size: 20px;
-  font-weight: normal;
   font-family: Helvetica;
   line-height: 1.5;
   text-align: center;
