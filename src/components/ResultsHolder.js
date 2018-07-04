eccd0c7a82baab871283da574aa2a37841549c58
Joshua Holloway <josh.holloway@live.co.uk>
2018-07-04 15:39:16 +0100

    :building_construction: architectural changes. now it just renders cheese, all logic extracted

--- src/components/ResultsHolder.js
+++ src/components/ResultsHolder.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import ResultsStyle from './styledComponents/Results';
-import { cheeseBlockCalculator, currencyConverter} from "./MiscellaneousFunctions";
 
 const ResultsHolder = (props) => {
 
-  const { state } = props;
+  const {amountOfCheese} = props;
 
-  if (!state || Object.keys(state.results).length === 0) {
-    return <ResultsStyle>The number of blocks of cheese you can afford will appear here...</ResultsStyle>
-  }
-
-    currencyConverter(state);
-
-    cheeseBlockCalculator(state);
-
-    const cheeseBlocks = cheeseBlockCalculator(state);
-
-    return (
-      <div>
-        <ResultsStyle>
-          {Math.floor(cheeseBlocks)} blocks of cheese!
-        </ResultsStyle>
-      </div>
-    )
+  return amountOfCheese ? (
+    <ResultsStyle>
+      {Math.floor(amountOfCheese)} blocks of cheese!
+    </ResultsStyle>
+  ) : (
+    <ResultsStyle>
+      The number of blocks of cheese you can afford will appear here...
+    </ResultsStyle>
+  )
 };
 
 export default ResultsHolder;
